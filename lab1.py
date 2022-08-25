530438a9ee9ffd8583b62c49c2c737d9b3876ae9
samanto1969 <fns0404@prodigy.net.mx>
2022-08-25 14:53:37 -0500

    bueno

--- lab1.py
+++ lab1.py
@@ -1,6 +1,40 @@
-#================
-#Samanto
-#===============
 
-print (2+5)
+
+
+
+#=============================================================================
+# Fernando Nicasio 23 de agosto de 2022
+#=============================================================================|
+
+print (2+3)
+print (2*3)
+print (2/3)
+print (2**10)
+print (2**0.5) # raíz cuadrada
+print (10%2)
+print (10%0.1) # exclusivo en pytho
+
+#==============================================================================
+# Para saber el tipo de objeto se usa type
+#==============================================================================
+
+t=0 
+print (type(t)) # entero
+t=3.6
+print (type(t)) # real (flotante)
+t=True
+print (type(t)) # booleano (bool)
+
+
+#==============================================================================
+# Mensaje a pantalla
+#==============================================================================
+
+
+print ("Este es un conando de python. ", "Este es otro enunciado.", t)
+print ('id: ', 1)
+print ('First Name: ', 'Steve')
+print ('Last Name: ', 'Jobs')
+print ("Vamos a sumar esto" + "con esto otro")
+
 
