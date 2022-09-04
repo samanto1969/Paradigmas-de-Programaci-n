96154fa842ae2467c7042d613538ebe703cb9392
samanto1969 <fns0404@prodigy.net.mx>
2022-09-03 22:43:23 -0500

    Segundo video de Mitote Ciencia

--- lab2.py
+++ lab2.py
@@ -118,9 +118,84 @@ print(romanNums.keys())
 print(romanNums.values())
 
 #   Juicio de llave (está o no está la llave en el diccionario)
-print("I" in rimanNums)
-print("X" in rimanNums)
-print("XX" not in rimanNums)
+print("I" in ronanNums)
+print("X" in romanNums)
+print("XX" not in romanNums)
+
+#=============================================================
+#   Listas
+#   Las listas pueden ser de objetos diferentes
+#=============================================================
+
+miprimeralista = [] # Lista vacia
+print(miprimeralista)
+
+#=============================================================
+#   Llenado de lista
+#=============================================================
+
+miprimeralista = [1,"Javier",1.34,"Bosco","Angel","Abigail",True]
+print(miprimeralista)
+
+#===============================================================
+#   list: hacer una lista
+#   range(i,j): secuencia de i hasta j-1
+#===============================================================
+nums = list(range(1,61))
+for i in nums:
+    print(i)
+
+#===============================================================
+#   Inclúir nuevos elementos en la lista
+#===============================================================
+nums.append(61)
+nums.append(62)
+nums.append(61)
+print(nums)
+
+#===============================================================
+#   Quitar elemento de la lista
+#===============================================================
+nums.remove(61)
+print(nums)
+
+#==============================================================
+#   Quitar elemento con índice i
+#==============================================================
+i = 61
+del nums[i]
+print(nums)
+
+#===============================================================
+#   Borrar la  lista
+#===============================================================
+del nums
+
+#==============================================================
+#   Sumar listas
+#==============================================================
+L1 = [1,2,3]
+L2 = [4,5,6]
+print(L1+L2]
+
+#==============================================================
+#   Llenado a mano
+#==============================================================
+potencial = []
+for i in range(0,10000):
+    potencial.append(float(i))
+print(potencial[100])
+
+#==============================================================
+#   Generar una tupla con la lista
+#==============================================================
+potencial = tuple(potencial)
+print(potencial[100]
+
+
+
+
+
 
 
 
