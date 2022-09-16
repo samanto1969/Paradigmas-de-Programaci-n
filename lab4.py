5db98ea0b4493537a05e16fecaae9953b756eb4e
samanto1969 <fns0404@prodigy.net.mx>
2022-09-16 14:51:44 -0500

    Programa 4 del video 4 de Mitote Ciencia

--- lab4.py
+++ lab4.py
@@ -207,4 +207,64 @@ class B:
 
 #============================================================
 #   ASOCIACIÓN
+#===========================================================
+#   Usando objetos independientes
+objetoA = A(1.0,2.0,3.0)
+objetoB = B(4.0,5.0)
+
+print(objetoB.sumar_todo(objetoA.a,objetoA.b))
+
 #============================================================
+#   El objeto C tiene dos reales y un objeto A
+#   El objeto A se instanci dentro de C
+#===========================================================
+class C:
+    __d:float=0.0
+    __e:float=0.0
+    __Aa:A=None
+
+    def __init__(self,d:float,e:float):
+        self.d = d
+        self.e = e
+        #   A está instanciado adentro
+        self.Aa = A(1.0,2.0,3.0)
+
+    def sumar_todo(self):
+        x:float=self.d+self.e+self.Aa.a+self.Aa.b
+        return x
+
+#================================================================
+#   COMPOSICIÓN
+#   Contiene otro objeto dentro
+#================================================================
+objetoC = C(4.0,5.0)
+print(objetoC.sumar_todo())
+
+#================================================================
+#   Objeto D tiene dos reales y un objeto A
+#   definido por fuera
+#================================================================
+class D:
+    __d:float=0.0
+    __e:float=0.0
+    __Aa:A=None
+
+    def __init__(self,d:float,e:float,Aa:A):
+        self.d = d
+        self.e = e
+        self.Aa = Aa
+
+    def sumar_todo(self):
+        x:float=self.d+self.e+self.Aa.a+self.Aa.b
+        return x
+
+#=================================================================
+#   AGREGACIÓN
+#   Construye el objeto agregado por fuera
+#=================================================================
+objetoD = D(4.0,5.0,objetoA)
+print(objetoD.sumar_todo())
+
+
+
+
