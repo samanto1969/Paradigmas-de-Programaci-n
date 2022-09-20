ab38cff7485a70fdcfcc326d36040f5bf5dcd432
samanto1969 <fns0404@prodigy.net.mx>
2022-09-20 14:56:37 -0500

    Programa 5 del video 5 de Mitote Ciencia

---
+++ lab5.py
@@ -0,0 +1,14 @@
+###########################################################################
+#   Fernando Nicasiio Ramírez
+#   Programa 5 del Video 5 de Mitote Ciencia
+##########################################################################
+
+#   De functions.py traer las funciones suma y promedio
+
+##########################################################################
+
+from functions import suma,promedio
+print(suma(100,200))
+print(promedio(100,200))
+
+
