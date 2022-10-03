3d206dedd223beab83a058f43fd3f779eeb1435c
samanto1969 <fns0404@prodigy.net.mx>
2022-10-02 21:19:24 -0500

    Programa 5 del video 5 de Mitote Ciencia

--- lab5.py
+++ lab5.py
@@ -17,6 +17,7 @@ print('!Que hermoso día')
 print('Aqui tratando de nuevo')
 print('Hola de nuevo miercoles 29 de septiembre de 2022')
 print('Más de este miercoles 28 de septiembre de 2022')
+print("Ahora con Jupyter Notebook")
 
 
 
