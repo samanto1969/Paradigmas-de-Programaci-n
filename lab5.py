1010c6cfda8b3e68488376219d3bbe0f0b302294
samanto1969 <fns0404@prodigy.net.mx>
2022-10-06 16:33:04 -0500

    Programa 5 del video 5 de Mitote Ciencia

---
+++ lab5.py
@@ -0,0 +1,26 @@
+###########################################################################
+#   Fernando Nicasiio Ramírez
+#   Programa 5 del Video 5 de Mitote Ciencia
+##########################################################################
+
+#   De functions.py traer las funciones suma y promedio
+
+##########################################################################
+
+from functions import suma,promedio
+print(suma(100,200))
+print(promedio(100,200))
+
+print('buenas noches')
+print('Hasta mañana')
+print('!Que hermoso día')
+print('Aqui tratando de nuevo')
+print('Hola de nuevo miercoles 29 de septiembre de 2022')
+print('Más de este miercoles 28 de septiembre de 2022')
+print("Ahora con Jupyter Notebook")
+print('Nuevamente clonando github con jupyter notebook con windows')
+
+
+
+
+
