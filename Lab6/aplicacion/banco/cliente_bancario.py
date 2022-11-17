b8604237da4595283ee7b2b20efb00cd79d63124
samanto1969 <fns0404@prodigy.net.mx>
2022-11-17 15:59:27 -0600

    Laboratorio 6

---
+++ Lab6/aplicacion/banco/cliente_bancario.py
@@ -0,0 +1,63 @@
+#==========================================
+# Clase ClienteBancario
+#==========================================
+class ClienteBancario:
+    __nombres:str = None
+    __apellidos:str = None
+    __edad:int = 0
+    __balanceDeCuenta:float = 0.0
+
+
+    def __init__(self,nombres:str, apellidos:str, edad:int=0, balanceDeCuenta:float=0.0):
+        self.__validaEdad(edad)
+        self.__validaCantidad(balanceDeCuenta)
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.__edad = edad
+        self.balanceDeCuenta = balanceDeCuenta
+
+    def getNombreCompleto(self) -> str:
+        return self.nombres + "  " + self.apellidos
+
+
+    def __mandaEmail(self, titulo:str, texto:str) -> None:
+        print("mandar email: " + titulo + "con texto: " + texto)
+
+
+    def __enviarBalanceAlBanco(self, cantidad:float) -> None:        
+        print("Enviando cantidad: " + str(cantidad) + "al banco...")
+
+#======================================================================
+# Método privado con dos guiones bajos
+# Si la edad es menor que 18 genera un error
+#======================================================================
+
+    def __validaEdad(self, edad:int) -> None:
+        if edad < 18:
+            raise Exception("ES menor de edad")
+
+    def imprimirInfo(self) -> str:
+        return "Nombre: " + self.getNombreCompleto() + ", Edad: " + str(self.__edad) + ", Balance: "  + str(self.__balanceDeCuenta)
+
+
+    #===================================================================
+    # Método privado que checa si el balance es negativo
+    # y genera un error
+    #===================================================================
+    def __validaCantidad(self, balanceDeCuenta:float) -> None:
+        if balanceDeCuenta < 0:
+            raise Exception("El balance en la cuenta no puede ser negativo")
+
+    def guardarDinero(self, cantidad:float) -> None:
+        self.__balanceDeCuenta = self.__balanceDeCuenta + cantidad
+        self.__mandarEmail("---- guardando depósito ----", " se recibieron " + str(cantidad))
+        self.__enviarBalanceAlBanco(cantidad)
+
+
+    def retirarDinero(self, cantidad:float) -> None:
+        cantidadFinal = self.__balanceDeCuenta - cantidad
+        self.__validaCantidad(cantidadFinal)
+        self.__balanceDeCuenta = cantidadFinal
+        self.__mandarEmail("---- retirando dinero ----", " se retiró " + str(cantidad))
+        self.__enviarBalanceAlBanco(cantidad)
+
---
+++ Lab6/main.py
@@ -0,0 +1,39 @@
+from aplicacion.banco.cliente_bancario import ClienteBancario
+
+#==========================================================
+# try: intente (correr las instruccione)
+# except: atrapar el error en una variable e
+# e se puede convertir a string
+#=============================================================
+
+
+#============================================================
+# Error por sacar más dinero del que tiene
+#============================================================
+try:
+    cliente = ClienteBancario("Jaime Andrade", "Hernandez Sánchez", 28, 0.0)
+    cliente.guardarDinero(300)
+    print(cliente.imprimirInfo())
+    cliente.retirarDinero(400)
+    print(cliente.imprimirInfo())
+
+#============================================================================
+# Exception es el objeto más general de error
+#============================================================================
+except Exception as e:
+    print("Error: " + str(e))
+
+
+#============================================================================
+# Error por usar un atributo privado
+#============================================================================
+try:
+  print(cliente.__nombres)
+except Exception as ex:
+  print("Error: " + str(ex))
+
+#============================================================================
+# Forma correcta
+#============================================================================
+#print(cliente.nombres)
+
