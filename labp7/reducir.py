f35ffb8481c7f81fc40d17c98871846c2608beb1
samanto1969 <fns0404@prodigy.net.mx>
2022-11-24 14:24:24 -0600

    Laboratorio 7

---
+++ labp7/reducir.py
@@ -0,0 +1,26 @@
+#=======================================
+# Uso de reduccion (colapsar resultados)
+#=======================================
+
+#=================================================
+# Multiplicacion de todos los numeros en la lista
+#=================================================
+
+from functools import reduce
+
+bigdata = [2,3,5,7,11,13,17,19,23,29]
+
+#===============
+# Funciones x*y
+#===============
+
+multiplicar = lambda x,y: x*y
+
+print(reduce(multiplicar,bigdata))
+
+#==========================================================
+# Reduce le aplica la funcion por pares a los resultados y
+# el siguiente elemento comenzando con los dos primeros
+# elementos
+#==========================================================
+
